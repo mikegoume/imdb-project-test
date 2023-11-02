a73a2bf76e7a4c020d6a1edfbf79bc8d0403cdbf
mike_goume <mike.goume@example.com>
2023-11-02 18:12:27 +0200

    Change the style of Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,17 +5,19 @@ const Navbar = (props) => {
   const { onLogout } = props;
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="w-4/5 flex flex-wrap items-center justify-between mx-auto pt-4 pb-4">
-        <a href="#" className="flex items-center">
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+    <nav className="bg-darknavy h-20 flex items-center">
+      <div className="w-4/5 h-full flex items-center justify-between mx-auto">
+        <a href="#" className="">
+          <span className="font-sans text-white font-extrabold self-center text-2xl">
             Movies
+            <span className="font-serif text-teal text-4xl">4</span>
+            you
           </span>
         </a>
         <button
           data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
           aria-controls="navbar-default"
           aria-expanded="false"
         >
@@ -23,7 +25,7 @@ const Navbar = (props) => {
           <svg
             className="w-6 h-6"
             aria-hidden="true"
-            fill="currentColor"
+            fill="white"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
@@ -34,41 +36,37 @@ const Navbar = (props) => {
             ></path>
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link
-                to="/"
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-              >
+        <div
+          className="hidden h-full w-full md:flex md:w-auto md:items-center"
+          id="navbar-default"
+        >
+          <ul className="h-full items-center flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0">
+            <li className="h-full hover:bg-green flex items-center px-4">
+              <Link to="/" className="block text-white text-lg">
                 Now Playing
               </Link>
             </li>
-            <li>
-              <Link
-                to="/popular"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
+            <li className="h-full hover:bg-green flex items-center px-4">
+              <Link to="/popular" className="block text-white text-lg">
                 Popular
               </Link>
             </li>
-            <li>
-              <Link
-                to="/top-rated"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
+            <li className="h-full hover:bg-green flex items-center px-4">
+              <Link to="/top-rated" className="block text-white text-lg">
                 Top Rated
               </Link>
             </li>
-            <li>
-              <Link
-                to="/upcoming"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
+            <li className="h-full hover:bg-green flex items-center px-4">
+              <Link to="/upcoming" className="block text-white text-lg">
                 Upcoming
               </Link>
             </li>
-            <button onClick={() => onLogout()}>Logout</button>
+            <button
+              className="bg-green py-2 px-5 text-white rounded-md text-lg"
+              onClick={() => onLogout()}
+            >
+              Logout
+            </button>
           </ul>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      white: "#fff",
+      black: "#0F0F0F",
+      darknavy: "#232D3F",
+      green: "#005B41",
+      teal: "#008170",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        "8xl": "96rem",
+        "9xl": "128rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
   },
   plugins: [],
 };
